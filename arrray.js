bdaeb04e3cb238c5f88df55565ddb5cc01ad451f
Trevor <trvrmwangi@gmail.com>
2025-01-07 11:30:26 +0300

    sort array

---
+++ arrray.js
@@ -0,0 +1,9 @@
+const arr = [5,2,7,1,9,3,4,6,8,10]
+
+function delayedSort(arr) {
+    const sorted = [...arr].sort((a,b) => a-b); 'sort the array'
+    sorted.forEach((num, index) => {
+        setTimeout(( ) => console.log(num), index * 500);
+    });
+}
+delayedSort(arr);
